634c08a91547b1e8f2ebe87db095d730017dfc52
Michell Cronberg <admin@TA-828-INS>
2018-10-29 18:26:51 +0100

    .

---
+++ VariabelDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariabelDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int a;
+            int b = 10;
+            
+
+            
+
+
+            bool c;
+
+            
+
+            bool d = false;
+
+            int e, f, g;
+
+            byte h = 50;
+
+            float y = Convert.ToSingle(0.44423);
+
+
+            int dd = 132;
+            double ffg = 394.24345;
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
